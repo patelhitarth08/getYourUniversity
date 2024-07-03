433c14c873fae69503cca83efd1016995eae67cb
Hitarth <patelhitarth08@gmail.com>
2024-07-03 17:27:39 +0530

    updated readme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="shortcut icon" href="/images/next.svg" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
